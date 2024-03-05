f4ba75859a4b3c8f5dda559e9ca4f186d2ca2a2f
CA Icatar <themadgarlic@gmail.com>
2024-03-05 14:57:19 +0800

    fixed load_id
    fixed query for load_id

--- app/models/fms_g16_shipment.rb
+++ app/models/fms_g16_shipment.rb
@@ -1,5 +1,5 @@
 class FmsG16Shipment < ApplicationRecord
   belongs_to :fms_g16_route
-  has_many :fms_g16_loads, foreign_key: 'fms_g16_shipment_load_id'
+  has_many :fms_g16_loads, foreign_key: 'fms_g16_shipment_id'
   paginates_per 10
 end
